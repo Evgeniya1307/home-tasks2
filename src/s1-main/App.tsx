12828b0fdb2db8984676626852179f7cf9bff948
Evgeniya13 <zxzxzx1307@gmail.com>
2024-08-29 12:58:43 -0600

    {/* Затемнение справа от открытого меню */}             {open && <div className={s.background} onClick={handleClose} />}

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import s from './App.module.css'
-import HW1 from '../s2-homeworks/hw01/HW1';
-import HW2 from '../s2-homeworks/hw02/HW2';
-import HW3 from '../s2-homeworks/hw03/HW3';
-import HW4 from '../s2-homeworks/hw04/HW4'; // Подключаем HW4
+import React from 'react';
+import HW5 from '.././s2-homeworks/hw05/HW5';
+// import HW1 from './components/hw01/HW1';
+// import HW2 from './components/hw02/HW2';
+// import HW3 from './components/hw03/HW3';
+// import HW4 from './components/hw04/HW4';
 
 function App() {
     return (
-        <div className={s.App}>
-            <HW1/>
-            <HW2/>
-            <HW3/>
-            <HW4/> {/* Добавляем HW4 для выполнения домашнего задания */}
+        <div>
+            {/* Закомментированы предыдущие задания, так как они уже включены в HW5 */}
+            {/* <HW1 /> */}
+            {/* <HW2 /> */}
+            {/* <HW3 /> */}
+            {/* <HW4 /> */}
+            <HW5 />
         </div>
-    )
+    );
 }
 
 export default App;
--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -1,35 +1,37 @@
-import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Error404 from './pages/Error404'
-import PreJunior from './pages/PreJunior'
-import Junior from './pages/Junior'
-import JuniorPlus from './pages/JuniorPlus'
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Error404 from './pages/Error404';
+import PreJunior from './pages/PreJunior';
+import Junior from './pages/Junior';
+import JuniorPlus from './pages/JuniorPlus';
 
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
     JUNIOR: '/junior',
     JUNIOR_PLUS: '/junior-plus',
-}
+};
 
 function Pages() {
     return (
         <div>
-            {/*Routes выбирает первый подходящий роут*/}
             <Routes>
-                {/*роутинг будут писать студенты*/}
-                {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                {/*<Route ...*/}
+                {/* Когда попадаем на '/', перенаправляем сразу на страницу '/pre-junior' */}
+                <Route path="/" element={<Navigate to={PATH.PRE_JUNIOR} />} />
 
-                {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
+                {/* Роут для страницы PreJunior */}
+                <Route path={PATH.PRE_JUNIOR} element={<PreJunior />} />
 
-                {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                {/*<Route ...*/}
+                {/* Роут для страницы Junior */}
+                <Route path={PATH.JUNIOR} element={<Junior />} />
+
+                {/* Роут для страницы Junior Plus */}
+                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus />} />
+
+                {/* Роут для отображения страницы ошибки 404 */}
+                <Route path="*" element={<Error404 />} />
             </Routes>
         </div>
-    )
+    );
 }
 
-export default Pages
+export default Pages;
