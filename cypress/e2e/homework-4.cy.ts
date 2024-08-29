af966ee07ccdb8c659b6bf2a558bad8fc8356aa4
Evgeniya13 <zxzxzx1307@gmail.com>
2024-08-29 12:36:58 -0600

    Fixed checkbox logic and updated Cypress tests

--- cypress/e2e/homework-4.cy.ts
+++ cypress/e2e/homework-4.cy.ts
@@ -1,19 +1,17 @@
 describe('Checkbox testing', () => {
   it('should check the first checkbox', () => {
-    cy.get('#hw4-super-checkbox-with-text', { timeout: 10000 })
+    cy.get('#hw4-super-checkbox-with-text')
       .should('exist')
-      .should('be.visible')
       .should('not.be.checked')
-      .check({ force: true })
+      .click()  // Используем клик вместо check
       .should('be.checked');
   });
 
   it('should uncheck the second checkbox', () => {
-    cy.get('#hw4-super-checkbox-like-old', { timeout: 10000 })
+    cy.get('#hw4-super-checkbox-like-old')
       .should('exist')
-      .should('be.visible')
       .should('be.checked')
-      .uncheck({ force: true })
+      .click()  // Используем клик вместо uncheck
       .should('not.be.checked');
   });
 });
